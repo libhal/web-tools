0e25f0d552e307f61a9ee80d22e09a5c13dedae9
Khalil Estell <kammce0@gmail.com>
2024-05-09 12:43:44 -0700

    :sparkles: Add web-can tool

--- can/serial.js
+++ can/serial.js
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2024 Khalil Estell
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,106 +12,111 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let port;
-let reader;
-let device_connected = false;
-const decoder = new TextDecoder("utf-8");
-const encoder = new TextEncoder("utf-8");
-
-function getBaudRate() {
-  let baudrate = document.querySelector("#baudrate").value;
-  if (baudrate === "custom") {
-    baudrate = document.querySelector("#baudrate_custom").value;
+class SerialDevice {
+  constructor() {
+    this.port = null;
+    this.reader = null;
+    this.writer = null;
+    this.onDataCallback = null;
+    this.baudRateSource = null;
+    this.onConnectCallback = null;
+    this.onDisconnectCallback = null;
+    this.connected = false;
   }
-  return Number(baudrate);
-}
 
-async function connectToDevice() {
-  try {
-    port = await navigator.serial.requestPort();
-
-    const baud_rate = getBaudRate();
-    await port.open({ baudRate: baud_rate });
-    await port.setSignals({ dataTerminalReady: false, requestToSend: false });
-    device_connected = true;
-    document.querySelector("#connect-btn").innerText = "Disconnect";
-    document.querySelector("#baudrate").setAttribute("disabled", true);
-    document.querySelector("#dtr-checkbox").removeAttribute("disabled");
-    document.querySelector("#rts-checkbox").removeAttribute("disabled");
-    readFromDevice();
-  } catch (error) {
-    const notFoundText = "NotFoundError: No port selected by the user.";
-    const userCancelledConnecting = String(error) === notFoundText;
-    if (!userCancelledConnecting) {
-      alert(`Could not connect to serial device: ${error}`);
+  // Method to request and open a serial port
+  async connect() {
+    if ("serial" in navigator) {
+      try {
+        this.port = await navigator.serial.requestPort();
+        await this.port.open({ baudRate: this.baudRateSource() });
+        await this.port.setSignals({
+          dataTerminalReady: false,
+          requestToSend: false,
+        });
+        this.writer = this.port.writable.getWriter();
+        this.reader = this.port.readable.getReader();
+        console.log("Connected to the serial port!");
+        this.startReading();
+        this.connected = true;
+        if (this.onConnectCallback) {
+          await this.onConnectCallback();
+        }
+      } catch (error) {
+        console.error("There was an error opening the serial port:", error);
+      }
+    } else {
+      console.error("WebSerial API not supported.");
     }
   }
-}
 
-async function readFromDevice() {
-  while (port.readable && device_connected) {
-    reader = port.readable.getReader();
-    try {
-      while (true) {
-        const { value, done } = await reader.read();
+  // Private method to start reading from the serial port
+  async startReading() {
+    while (this.port && this.port.readable) {
+      try {
+        const { value, done } = await this.reader.read();
         if (done) {
+          console.log("Reader stream closed");
+          this.reader.releaseLock();
           break;
         }
-        let decoded = new TextDecoder().decode(value);
-        decoded = decoded.replace(/\n/g, "\r\n");
-        term.write(decoded);
+        if (this.onDataCallback) {
+          this.onDataCallback(new TextDecoder().decode(value));
+        }
+      } catch (error) {
+        console.error("Error reading from serial port:", error);
+        this.reader.releaseLock();
+        break;
+      }
+    }
+  }
+
+  onData(callback) {
+    this.onDataCallback = callback;
+  }
+
+  onConnect(callback) {
+    this.onConnectCallback = callback;
+  }
+
+  onDisconnect(callback) {
+    this.onDisconnectCallback = callback;
+  }
+
+  setBaudRateSourceCallback(callback) {
+    this.baudRateSource = callback;
+  }
+
+  isConnected() {
+    return this.connected;
+  }
+
+  async setSignals(signals) {
+    await this.port.setSignals(signals);
+  }
+
+  async write(data) {
+    try {
+      const encoder = new TextEncoder("utf-8");
+      await this.writer.write(encoder.encode(data));
+    } catch (error) {
+      console.error("Failed to write to serial port:", error);
+    }
+  }
+
+  async disconnect() {
+    try {
+      this.reader && this.reader.releaseLock();
+      this.writer && (await this.writer.close());
+      this.port && (await this.port.close());
+      this.port = null;
+      console.log("Disconnected from the serial port");
+      this.connected = false;
+      if (this.onDisconnectCallback) {
+        this.onDisconnectCallback();
       }
     } catch (error) {
-      disconnectFromDevice();
-      console.error(error);
-    } finally {
-      reader.releaseLock();
+      console.error("Failed to close the serial port:", error);
     }
   }
-  await port.close();
-}
-
-async function writeToDevice(input) {
-  let cr = flags.get("carriage-return-checkbox") ? "\r" : "";
-  let nl = flags.get("newline-select") ? "\n" : "";
-  const payload = `${input}${cr}${nl}`;
-
-  if (port.writable) {
-    const writer = port.writable.getWriter();
-    try {
-      await writer.write(encoder.encode(payload));
-    } catch (error) {
-      console.error(error);
-    } finally {
-      writer.releaseLock();
-    }
-  }
-}
-
-async function writeCharacterToDevice(input) {
-  const payload = `${input}`;
-
-  if (port.writable) {
-    const writer = port.writable.getWriter();
-    try {
-      await writer.write(encoder.encode(payload));
-    } catch (error) {
-      console.error(error);
-    } finally {
-      writer.releaseLock();
-    }
-  }
-}
-
-function disconnectFromDevice() {
-  device_connected = false;
-  if (reader && reader.cancel) {
-    reader.cancel();
-  }
-  document.querySelector("#connect-btn").innerText = "Connect";
-  document.querySelector("#baudrate").removeAttribute("disabled");
-  document.querySelector("#dtr-checkbox").setAttribute("disabled", true);
-  document.querySelector("#rts-checkbox").setAttribute("disabled", true);
-  document.querySelector("#dtr-checkbox").removeAttribute("disabled");
-  document.querySelector("#rts-checkbox").removeAttribute("disabled");
 }
